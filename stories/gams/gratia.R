8f918f6e6de81130361edb1b03f1d16634baf4cc
Renata Diaz <diaz.renatam@gmail.com>
2020-10-23 15:12:23 -0400

    gam_rats.R includes a hacky way to get samples for the DERIVATIVE. will not work with poisson.

--- stories/gams/gratia.R
+++ stories/gams/gratia.R
@@ -1,29 +1,5 @@
 library(gratia)
 load_mgcv()
-# # \dontshow{
-# set.seed(42)
-# op <- options(cli.unicode = FALSE)
-# # }
-# dat <- gamSim(1, n = 400, dist = "normal", scale = 2, verbose = FALSE)
-# mod <- gam(y ~ s(x0), data = dat, method = "REML")
-#
-# ## first derivatives of all smooths using central finite differences
-# derivs <- derivatives(mod, type = "central")
-# #> # A tibble: 800 x 8
-# #>    smooth var       data derivative    se  crit lower upper
-# #>    <chr>  <chr>    <dbl>      <dbl> <dbl> <dbl> <dbl> <dbl>
-# #>  1 s(x0)  x0    0.000239       7.41  3.33  1.96 0.874  13.9
-# #>  2 s(x0)  x0    0.00525        7.41  3.33  1.96 0.875  13.9
-# #>  3 s(x0)  x0    0.0103         7.40  3.33  1.96 0.884  13.9
-# #>  4 s(x0)  x0    0.0153         7.40  3.31  1.96 0.902  13.9
-# #>  5 s(x0)  x0    0.0203         7.39  3.30  1.96 0.929  13.8
-# #>  6 s(x0)  x0    0.0253         7.38  3.27  1.96 0.965  13.8
-# #>  7 s(x0)  x0    0.0303         7.36  3.24  1.96 1.01   13.7
-# #>  8 s(x0)  x0    0.0353         7.34  3.20  1.96 1.07   13.6
-# #>  9 s(x0)  x0    0.0403         7.32  3.15  1.96 1.14   13.5
-# #> 10 s(x0)  x0    0.0453         7.29  3.10  1.96 1.21   13.4
-# #> # ... with 790 more rows# \dontshow{
-
 
 library(dplyr)
 library(ggplot2)
@@ -55,7 +31,7 @@ datasets <- data.frame(
                 "rtrg_26_59")
 )
 
-ibd <- readRDS(("C:\\Users\\diaz.renata\\Documents\\GitHub\\BBSsize\\analysis\\isd_data\\ibd_isd_bbs_rtrg_19_35.Rds"))
+ibd <- readRDS(("C:\\Users\\diaz.renata\\Documents\\GitHub\\BBSsize\\analysis\\isd_data\\ibd_isd_bbs_rtrg_304_17.Rds"))
 
 sv <- ibd %>%
   group_by(year) %>%
@@ -79,7 +55,7 @@ fitplot <- ggplot(abund_fit, aes(year, abundance)) +
   geom_point() +
   geom_line(aes(year, .value))
 
-derivs <- derivatives(nmod, n = 100)
+derivs <- derivatives(nmod, n = 200)
 
 derivplot <- ggplot(derivs, aes(data, derivative)) +
   geom_point() +
@@ -102,7 +78,7 @@ fitplot <- ggplot(mod_fit, aes(year, energy)) +
   geom_point() +
   geom_line(aes(year, .value))
 
-derivs <- derivatives(nmod, n = 100)
+derivs <- derivatives(nmod, n = 200)
 
 derivplot <- ggplot(derivs, aes(data, derivative)) +
   geom_point() +
@@ -115,17 +91,17 @@ gridExtra::grid.arrange(grobs = list(realplot, fitplot, derivplot), ncol = 1)
 
 #### biomass
 
-realplot <- ggplot(sv, aes(year, biomass)) + geom_line()
+realplot <- ggplot(sv, aes(year, scale(biomass))) + geom_line()
 
-nmod <-  gam(biomass ~ s(year), data = sv, method = "REML")
+nmod <-  gam(scale(biomass) ~ s(year), data = sv, method = "REML")
 
 mod_fit <- add_fitted(sv, nmod)
 
-fitplot <- ggplot(mod_fit, aes(year, biomass)) +
+fitplot <- ggplot(mod_fit, aes(year, scale(biomass))) +
   geom_point() +
   geom_line(aes(year, .value))
 
-derivs <- derivatives(nmod, n = 100)
+derivs <- derivatives(nmod, n = 200)
 
 derivplot <- ggplot(derivs, aes(data, derivative)) +
   geom_point() +
@@ -150,7 +126,7 @@ fitplot <- ggplot(mod_fit, aes(year, richness)) +
   geom_point() +
   geom_line(aes(year, .value))
 
-derivs <- derivatives(nmod, n = 100)
+derivs <- derivatives(nmod, n = 200)
 
 derivplot <- ggplot(derivs, aes(data, derivative)) +
   geom_point() +
@@ -161,3 +137,85 @@ derivplot <- ggplot(derivs, aes(data, derivative)) +
 
 gridExtra::grid.arrange(grobs = list(realplot, fitplot, derivplot), ncol = 1)
 
+
+# predicted_samples_df <- predicted_samples(nmod, n = 3, seed = 1977) %>%
+#   mutate(draw = as.factor(draw))
+#
+#
+#
+# ggplot(predicted_samples_df, aes(row, response, group = draw, color = draw)) +
+#   geom_line()
+
+#### playing with derivs ####
+# this is gross and I don't understand
+# stepwidth = 1
+#
+# fine_years <- data.frame(year = seq(min(sv$year), max(sv$year), by = stepwidth))
+#
+# fine_samples <- predicted_samples(nmod, n = 1, newdata = fine_years, seed = 1977)
+#
+# fine_samples_offset <- fine_samples %>%
+#   filter(row < max(row)) %>%
+#   mutate(next_response = fine_samples$response[-1]) %>%
+#   mutate(incremental_slope = (next_response - response) / stepwidth)
+#
+# sampleplot <- ggplot(fine_samples_offset, aes(row, response)) +
+#   geom_line()
+#
+# derivplot <- ggplot(fine_samples_offset, aes(row, incremental_slope)) +
+#   geom_line()
+#
+# gridExtra::grid.arrange(grobs = list(sampleplot, derivplot), ncol = 1)
+
+
+nmod <-  gam(scale(energy) ~ s(year), data = sv, method = "REML")
+
+derivs <- derivatives(nmod, n = 200)
+
+head(derivs)
+
+derivs_fit <- derivs %>%
+  select(data, derivative) %>%
+  rename(year = data) %>%
+  mutate(abs_derivative = abs(derivative))
+
+derivs_increments <- derivs_fit$year[ 2:nrow(derivs_fit)] - derivs_fit$year[1:(nrow(derivs_fit)-1)]
+
+net_change <- sum(derivs_fit$derivative) * mean(derivs_increments)
+
+mean_slope <- net_change / (max(derivs_fit$year) - min(derivs_fit$year))
+
+mean(derivs_fit$derivative)
+
+total_change <- sum(derivs_fit$abs_derivative) * mean(derivs_increments)
+
+mean_magnitude <- total_change / (max(derivs_fit$year) - min(derivs_fit$year))
+
+median(derivs_fit$derivative)
+median(derivs_fit$abs_derivative)
+hist(derivs_fit$derivative)
+hist(derivs_fit$abs_derivative)
+
+#
+# derivs_sim <- derivs %>%
+#   mutate(derivative = rnorm(1, mean = derivative, sd = ))
+#   select(data, derivative) %>%
+#   rename(year = data) %>%
+#   mutate(abs_derivative = abs(derivative))
+#
+# derivs_increments <- derivs_sim$year[ 2:nrow(derivs_sim)] - derivs_sim$year[1:(nrow(derivs_sim)-1)]
+#
+# net_change <- sum(derivs_sim$derivative) * mean(derivs_increments)
+#
+# mean_slope <- net_change / (max(derivs_sim$year) - min(derivs_sim$year))
+#
+# mean(derivs_sim$derivative)
+#
+# total_change <- sum(derivs_sim$abs_derivative) * mean(derivs_increments)
+#
+# mean_magnitude <- total_change / (max(derivs_sim$year) - min(derivs_sim$year))
+#
+# median(derivs_sim$derivative)
+# median(derivs_sim$abs_derivative)
+# hist(derivs_sim$derivative)
+# hist(derivs_sim$abs_derivative)
