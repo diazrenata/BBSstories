d25925fa17aa40a1da71f9e73d7bf087105d0bc6
Renata Diaz <renatadiaz@Renatas-MacBook-Air.local>
2020-10-27 12:02:45 -0400

    bird sites - rough

--- gams/gam_fxns/wrapper_fxns.R
+++ gams/gam_fxns/wrapper_fxns.R
@@ -3,11 +3,18 @@ source(here::here("gams", "gam_fxns", "fd_fxns.R"))
 # Given a TS, fit a Poisson GAM with k = 5
 # Extract the derivatives
 
-mod_wrapper <- function(ts) {
+mod_wrapper <- function(ts, response_variable = c("abundance", "energy", "biomass"), identifier = c("species", "site_name"), k = 3) {
 
-  ts_mod <- gam(abundance ~ s(year, k = 3), data = ts, method = "REML", family = "poisson")
+  response <- (match.arg(response_variable))
+  ts_id <- match.arg(identifier)
 
-  ts_mod$species <- ts$species[1]
+  ts <- ts %>%
+    dplyr::rename(dependent = response,
+                  identifier = ts_id)
+
+  ts_mod <- gam(dependent ~ s(year, k = k), data = ts, method = "REML", family = "poisson")
+
+  ts_mod$identifier <- ts$identifier[1]
 
 
   return(ts_mod)
@@ -18,8 +25,8 @@ fit_wrapper <- function(mod) {
   ts_fit <- add_fitted(model = mod, data = mod$model)
 
   ts_fit <- ts_fit %>%
-    mutate(species = mod$species) %>%
-    rename(fitted_abundance = .value)
+    mutate(identifier = mod$identifier) %>%
+    rename(fitted_value = .value)
 
   return(ts_fit)
 
@@ -34,7 +41,7 @@ deriv_wrapper <- function(mod, seed_seed = NULL) {
 
   ts_derivs <- get_many_fd(mod, eps = .1, seed_seed = seed_seed)
 
-  ts_derivs$species <- mod$species
+  ts_derivs$identifier <- mod$identifier
 
   return(ts_derivs)
 }
@@ -49,7 +56,7 @@ derivs_summary <- function(derivs_df) {
 
 
   derivs_summary <- derivs_df %>%
-    group_by(seed, species) %>%
+    group_by(seed, identifier) %>%
     summarize(net_change = sum(increment),
               abs_change = sum(abs_increment)) %>%
     mutate(abs_v_net_change = log(abs(abs_change / net_change)))
@@ -61,7 +68,7 @@ sign_summary <- function(derivs_df) {
   nincrements <- length(unique(derivs_df$year))
 
   sign_df <- derivs_df %>%
-    select(year, lower, upper, eps, species) %>%
+    select(year, lower, upper, eps, identifier) %>%
     distinct() %>%
     group_by_all() %>%
     mutate(ci_sign = ifelse(all(upper < 0, lower < 0), "negative",
@@ -71,7 +78,7 @@ sign_summary <- function(derivs_df) {
     summarize(proportion_of_time = dplyr::n() / nincrements) %>%
     mutate(proportion_of_time = ifelse(is.na(proportion_of_time), 0, proportion_of_time)) %>%
     tidyr::pivot_wider(names_from = ci_sign, values_from = proportion_of_time, values_fill = 0) %>%
-    mutate(species = derivs_df$species[1])
+    mutate(identifier = derivs_df$identifier[1])
 
   return(sign_df)
 }
